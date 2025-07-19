874542faf2b84bcdbbc8693864866b39ad12943f
squallerQ <john199502031@gmail.com>
2025-07-19 12:22:54 +0200

    init: start unit-testing task

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/main.{ts,tsx}',
+        'src/setupTests.{ts}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
